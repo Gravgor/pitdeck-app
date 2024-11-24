c0bc72408b9bfc023ec61a65f45e4c549df8e25f
Marceli Borowczak <marcelborowczak@outlook.com>
2024-11-24 09:59:31 +0100

    fix: ea

--- src/components/layout/EA.tsx
+++ src/components/layout/EA.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function EA() {
     return (
-        <div className="min-h-12 py-2 bg-yellow-500/10 border-b border-yellow-500/20 flex items-center">
+        <div className="min-h-4 py-2 bg-yellow-500/10 border-b border-yellow-500/20 flex items-center">
             <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col sm:flex-row items-center justify-center gap-1.5 sm:gap-2 text-center">
                     <div className="flex items-center gap-1.5 sm:gap-2">
