48a4df729a126165268f68c68fba1e0eea861f3a
Marceli Borowczak <marcelborowczak@outlook.com>
2024-11-24 13:38:14 +0100

    fix navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -219,7 +219,7 @@ export function Navbar() {
                 </Link>
                 <Link
                   onClick={() => setIsMobileMenuOpen(false)}
-                  href="/auth/signup"
+                  href="/auth/register"
                   className="px-4 py-2 text-sm bg-red-600 hover:bg-red-700 text-white rounded-full transition-colors"
                 >
                   Get Started
