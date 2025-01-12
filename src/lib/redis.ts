0a238f2028f7c0433cbfd6b53dec3ab65249b76d
Marceli Borowczak <marceliborowczak@gmail.com>
2025-01-12 16:05:23 +0100

    fix for network

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from 'ioredis';
 
 // Create Redis client
-const redis = new Redis(process.env.REDIS_URL || 'redis://default:cZsmGewRpPPVmkHYgimVsECnhOXsoWRd@autorack.proxy.rlwy.net:45234');
+const redis = new Redis(process.env.REDIS_URL + '?family=0' || 'redis://default:cZsmGewRpPPVmkHYgimVsECnhOXsoWRd@autorack.proxy.rlwy.net:45234');
 
 export { redis }; 
