518e800c771cfed32913706a82fb163c1901e4a6
Marceli Borowczak <marceliborowczak@gmail.com>
2025-01-15 01:30:37 +0100

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // Add this function to get truly random records
 export async function getRandomRecords<T>(
