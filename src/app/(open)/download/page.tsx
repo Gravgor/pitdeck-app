f1d406ee05073baaddfea28c3d2dcec3047bd63e
Marceli Borowczak <marceliborowczak@gmail.com>
2025-01-20 22:22:31 +0100

    del

--- src/app/(open)/download/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DownloadPage } from '@/components/download/DownloadPage';
-import { WaitlistPage } from '@/components/download/WaitlistPage';
-
-export default function Download() {
-  // You can toggle this based on your needs
-  const isPreRelease = true;
-  
-  return <WaitlistPage />;
-} 
